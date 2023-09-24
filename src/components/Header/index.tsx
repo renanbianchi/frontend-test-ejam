8ef0bb84b5215ad664f655dfa12297e4ba8365f0
renan <renanbianchi@gmail.com>
2023-09-23 23:36:36 -0300

    fix: adjusted size to start showing mobile visuals

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,20 +7,20 @@ import Norton from '../../../assets/Logos/norton-antivirus.svg'
 
 export function Header() {
   return (
-    <Container className="flex justify-between p-5 sm:justify-between sm:py-[1.875rem] sm:px-[7.938rem] text-black">
+    <Container className="flex justify-between p-5 xl:justify-between xl:py-[1.875rem] xl:px-[7.938rem] text-black">
       <Image
-        className="w-[106.67px] h-[20px] sm:w-auto sm:h-auto"
+        className="w-[106.67px] h-[20px] xl:w-auto"
         src={Clarifion_logo}
         alt="Clarifion logo"
       />
       <div className="flex justify-center items-center gap-8">
         <Image
-          className="w-[44px] h-[16px] xl:w-auto sm:h-auto"
+          className="w-[44px] h-[16px] xl:w-auto xl:h-auto"
           src={McAfee}
           alt="McAfee logo"
         />
         <Image
-          className="w-[44px] h-[20px] xl:w-auto sm:h-auto"
+          className="w-[44px] h-[20px] xl:w-auto xl:h-auto"
           src={Norton}
           alt="Norton logo"
         />
