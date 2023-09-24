92dd2852145e912b20eac32d0dd86b2e659b6e3a
renan <renanbianchi@gmail.com>
2023-09-23 23:37:00 -0300

    fix: remove background from mobile version

--- src/components/OfferDetails/index.tsx
+++ src/components/OfferDetails/index.tsx
@@ -5,7 +5,7 @@ import { Contents } from '../Contents'
 export function OfferDetails() {
   return (
     <Container className="flex xl:flex-row flex-col justify-center xl:items-center">
-      <div className="flex xl:w-fit xl:flex-row flex-col-reverse justify-center gap-10 xl:p-10 p-5 xl:my-10  bg-[#FAFAFA]">
+      <div className="flex xl:w-fit xl:flex-row flex-col-reverse justify-center gap-10 xl:p-10 p-5 xl:my-10  xl:bg-[#FAFAFA]">
         <Contents className="hidden xl:flex xl:flex-col" />
         <ProductDetails />
       </div>
